1f19a78a3a97c7dfd1ecdfce0ba6817276bfccd2
Matokosp <jmsalinas@uc.cl>
2019-11-04 16:39:31 +0100

    favicon

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -45,6 +45,31 @@
 		}
 
 
+
+		// SCROLL WINDOW TO ID
+
+			// handle links with @href started with '#' only
+		$(document).on('click', 'a[href^="#"]', function(e) {
+		    // target element id
+		    var id = $(this).attr('href');
+
+		    // target element
+		    var $id = $(id);
+		    if ($id.length === 0) {
+		        return;
+		    }
+
+		    // prevent standard hash navigation (avoid blinking in IE)
+		    e.preventDefault();
+
+		    // top position relative to the document
+		    var pos = $id.offset().top;
+
+		    // animated top scrolling
+		    $('body, html').animate({scrollTop: pos});
+		});
+
+
 // PRUEBAS MENU
 
 // When the user scrolls the page, execute myFunction
@@ -124,29 +149,3 @@ function myFunction() {
 
 
 		})
-
-
-
-
-// SCROLL WINDOW TO ID
-
-	// handle links with @href started with '#' only
-$(document).on('click', 'a[href^="#"]', function(e) {
-    // target element id
-    var id = $(this).attr('href');
-
-    // target element
-    var $id = $(id);
-    if ($id.length === 0) {
-        return;
-    }
-
-    // prevent standard hash navigation (avoid blinking in IE)
-    e.preventDefault();
-
-    // top position relative to the document
-    var pos = $id.offset().top;
-
-    // animated top scrolling
-    $('body, html').animate({scrollTop: pos});
-});
